81a84195b60768538546f8e5d602b0b16548859a
ali <a.aloreabi2000@gmail.com>
2023-11-11 16:41:31 -0500

    pushing in projecting

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Full Stack Developlemt - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <h3>101386021</h3>
+        <p>Ali Al Aoraebi</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
